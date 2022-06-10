477d9c68983c77b0e918d4b680dd052bacd9f431
downloadtapin <67175505+downloadtapin@users.noreply.github.com>
2022-06-10 13:24:18 +0800

    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents $ git clone https://github.com/dts-kominfo-rea2a/challenge-07-mauliditapin2.git Cloning into 'challenge-07-mauliditapin2'... remote: Enumerating objects: 21, done. remote: Counting objects: 100% (21/21), done. remote: Compressing objects: 100% (18/18), done. remote: Total 21 (delta 3), reused 8 (delta 0), pack-reused 0 Receiving objects: 100% (21/21), 61.52 KiB | 1.43 MiB/s, done. Resolving deltas: 100% (3/3), done.
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents
    $ cd challenge-
    challenge-01-mauliditapin2/ challenge-05-mauliditapin2/
    challenge-02-mauliditapin2/ challenge-07-mauliditapin2/
    challenge-03-mauliditapin2/ challenge-git/
    challenge-04-mauliditapin2/
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents
    $ cd challenge-
    challenge-01-mauliditapin2/ challenge-05-mauliditapin2/
    challenge-02-mauliditapin2/ challenge-07-mauliditapin2/
    challenge-03-mauliditapin2/ challenge-git/
    challenge-04-mauliditapin2/
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents
    $ cd challenge-07-mauliditapin2/
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents/challenge-07-mauliditapin2 (main)
    $ code .
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents/challenge-07-mauliditapin2 (main)
    $ node index.js
    ReferenceError: Cannot access 'promiseTheaterIXX' before initialization
        at promiseOutput (C:\Users\annaila\Documents\challenge-07-mauliditapin2\prom
    ise.js:6:31)
        at C:\Users\annaila\Documents\challenge-07-mauliditapin2\index.js:5:21
        at Object.<anonymous> (C:\Users\annaila\Documents\challenge-07-mauliditapin2
    \index.js:7:3)
        at Module._compile (node:internal/modules/cjs/loader:1103:14)
        at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
        at Module.load (node:internal/modules/cjs/loader:981:32)
        at Function.Module._load (node:internal/modules/cjs/loader:822:12)
        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_ma
    in:77:12)
        at node:internal/main/run_main_module:17:47
    undefined
    ReferenceError: Cannot access 'promiseTheaterIXX' before initialization
        at promiseOutput (C:\Users\annaila\Documents\challenge-07-mauliditapin2\prom
    ise.js:6:31)
        at C:\Users\annaila\Documents\challenge-07-mauliditapin2\index.js:6:21
        at processTicksAndRejections (node:internal/process/task_queues:96:5)
    undefined
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents/challenge-07-mauliditapin2 (main)
    $ node index.js
    2
    4
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents/challenge-07-mauliditapin2 (main)
    $ git add .
    
    annaila@DESKTOP-H19JU79 MINGW64 ~/Documents/challenge-07-mauliditapin2 (main)
    $ git commit -m

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const TheaterIXX = await promiseTheaterIXX();
+    const TheaterVGC = await promiseTheaterVGC();
+    const semuaFilm = [...TheaterIXX, ...TheaterVGC];
+    const result = semuaFilm.filter((e) => e.hasil === emosi);
+    return result.length;
+  } catch (err) {
+    console.log(err);
+  }
+};;
 
 module.exports = {
   promiseOutput,
